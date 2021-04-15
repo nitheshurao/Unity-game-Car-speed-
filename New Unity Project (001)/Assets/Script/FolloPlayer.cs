2ab60fa5fc09ae2ca698c4cc141ec371e912834b
nithesh <nitheshurao@gmail.com>
2021-04-15 23:17:10 +0530

    Speed

---
+++ New Unity Project (001)/Assets/Script/FolloPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FolloPlayer : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 Offset = new Vector3(0.6f, 6.75f, -12);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + Offset;
+    }
+}
---
+++ New Unity Project (001)/Assets/Script/PlayerControler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float moveSpeed = 5.0f;
+    private float turnSpeed =23.0f;
+    private float HorizontalInput;
+    private float vertcalInput;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+//wre player input
+        vertcalInput = Input.GetAxis("Vertical");
+        HorizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * vertcalInput * moveSpeed);
+        transform.Rotate(Vector3.up * Time.deltaTime * HorizontalInput * turnSpeed);
+
+
+    }
+}
